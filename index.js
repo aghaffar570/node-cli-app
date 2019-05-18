61dd0dcde4dfe81475157013a973a956a4a1814a
Abdul Ghaffar <aghaffar570@gmail.com>
2019-05-18 13:30:17 -0400

    create data file from parsed data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // utils
-const { welcome, parse } = require('./lib');
+const { files, parse, welcome } = require('./lib');
 
 (async () => {
 
@@ -7,6 +7,16 @@ const { welcome, parse } = require('./lib');
   welcome()
 
   // fetch dataView
-  const data = await parse.fetchAndParsedData()
-  console.log('parsed data >> \n\n\n', data)
+  const data = await parse.fetchAndParseData();
+  const parseData = JSON.parse(data);
+
+
+  // store data into a file
+  if(!files.doesFileExist('data.json')) {
+    files.createAndSaveNewFile('data.json', data);
+  }
+
+
+
+  console.log('parsed data >> \n\n\n', parseData)
 })()
