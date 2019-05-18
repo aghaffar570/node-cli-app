6250c6280edf2583f1ba009d83960c087ff88ef1
Abdul Ghaffar <aghaffar570@gmail.com>
2019-05-18 14:10:15 -0400

    recieve input and display data from query

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // utils
-const { files, logic, parse, welcome } = require('./lib');
+const { files, logic, parse, prompt, welcome } = require('./lib');
 const chalk = require('chalk');
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 
 (async () => {
 
@@ -11,21 +21,52 @@ const chalk = require('chalk');
   const data = await parse.fetchAndParseData();
   const parseData = JSON.parse(data);
 
-
   // store data into a file
   if(!files.doesFileExist('data.json')) {
     files.createAndSaveNewFile('data.json', data);
   }
 
-  const resultData = logic.getViewDataFromFile(parseData, 'StackView')
+
+  rl.question('Enter (y/n) to begin query:', (start) => {
+    rl.setPrompt('Enter a selector :')
+    rl.prompt()
+    rl.on('line', (selectorInput) => {
+
+      const resultData = logic.getViewDataFromFile(parseData, selectorInput)
+      console.log(
+        resultData.length ? resultData : 'No such selector exist'
+      )
+
+      if(selectorInput.toLowerCase().trim() === 'n'
+      || selectorInput.toLowerCase().trim() === 'no') {
+        console.log('Goodbye')
+        rl.close()
+      }
+      else {
+        rl.setPrompt('Enter a selector :')
+        rl.prompt()
+      }
+    })
+
+  })
+
+
+
+
+
+
+
+
+
+  // const resultData = logic.getViewDataFromFile(parseData, 'StackView')
   // const resultData = logic.getViewDataFromFile(parseData, 'container')
   // const resultData = logic.getViewDataFromFile(parseData, 'videoMode')
 
-  console.log(
-    'search selector view >> \n\n\n',
-    resultData,
-    chalk.greenBright(`\n\n Total of ${ chalk.underline.magentaBright(resultData.length) } views \n\n`)
-  )
+  // console.log(
+  //   'search selector view >> \n\n\n',
+  //   resultData,
+  //   chalk.greenBright(`\n\n Total of ${ chalk.underline.magentaBright(resultData.length) } views \n\n`)
+  // )
 
 
 })()
